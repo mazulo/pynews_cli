c09be1317a1e6caba73a35c7a03c245b7398252d
Diego Garcia <drgarcia1986@gmail.com>
2016-10-20 21:47:24 -0200

    define entrypoints and add __main__.py (to execute with `python -m pynews`)

--- setup.py
+++ setup.py
@@ -2,10 +2,21 @@ import sys
 from setuptools import setup, find_packages
 
 
-long_description = """
-A Python CLI to browse news from Hacker News (and others, in the near future).
-Also is a Python implementation of the awesome hn-cli.
-"""
+def read(fname):
+    """ Return file content. """
+    with open(fname) as f:
+        content = f.read()
+
+    return content
+
+
+description = 'PyNews is a simple Python CLI to browse news from HN website'
+try:
+    long_description = read('README.rst')
+except IOError:
+    long_description = description
+
+
 author_name = 'Patrick Mazulo'
 author_email = 'pmazulo@gmail.com'
 dependencies = [
@@ -20,7 +31,7 @@ if sys.version_info.major == 2:
 setup(
     name='PyNews',
     version='0.1.3',
-    description='PyNews is a simple Python CLI to browse news from HN website',
+    description=description,
     long_description=long_description,
     url='https://github.com/mazulo/pynews_cli',
     author=author_name,
@@ -41,6 +52,6 @@ setup(
     download_url='https://github.com/mazulo/pynews_cli/archive/master.zip',
     packages=find_packages(exclude=['tests*']),
     install_requires=dependencies,
-    scripts=['bin/pynews'],
+    entry_points={'console_scripts': ['pynews = pynews.pynews:main']},
     platforms='windows linux',
 )
