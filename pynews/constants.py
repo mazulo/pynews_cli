8512689d88fd7b4162d3189b4dc1f41eb9e15ead
Patrick Mazulo <pmazulo@gmail.com>
2022-11-14 09:35:20 -0300

    Split code from pynews.py into other files

---
+++ pynews/constants.py
@@ -0,0 +1,4 @@
+import multiprocessing
+
+
+DEFAULT_THREADS_NUMBER = multiprocessing.cpu_count()
--- pynews/pynews.py
+++ pynews/pynews.py
@@ -1,82 +1,18 @@
-"""Script to gather news from HackerNews."""
-import argparse
-import multiprocessing
+"""
+Script to gather news from HackerNews.
+"""
 import sys
 
 import requests as req
 
+from .constants import DEFAULT_THREADS_NUMBER
+from .parser import get_parser_options
 from .utils import get_stories, create_list_stories, create_menu
-
-
-DEFAULT_THREADS_NUMBER = multiprocessing.cpu_count()
 
 
 def main():
     """Main entry point for the script."""
-    parser = argparse.ArgumentParser(
-        prog='PyNews-CLI',
-        description='Your news collector inside your terminal! Tell me, what\'s\
-                      cooler than that?',
-        usage="""
-        PyNews-CLI - News Collector from HackerNews API
-        Usage: pynews [-t/--top-stories number_of_stories]
-                      [-n/--news-stories number_of_stories]
-
-        If the number of stories is not supplied, will be showed 200 from the
-        500 stories.
-
-        Examples:
-        - Get Top Stories:
-            $ pynews -t 10 # or
-            $ pynews --top-stories 10
-            This will show the 10 first top stories from the list of 500.
-
-        - Get New Stories:
-            $ pynews -n 10 # or
-            $ pynews --news-stories
-            This will show the 10 first new stories from the list of 500.
-
-        Get basic options and Help, use: -h\--help
-
-        """,
-    )
-    parser.add_argument(
-        '-t',
-        '--top-stories',
-        nargs='?',
-        const=200,
-        type=int,
-        help='Get the top N stories from HackerNews API'
-    )
-
-    parser.add_argument(
-        '-n',
-        '--news-stories',
-        nargs='?',
-        const=200,
-        type=int,
-        help='Get the N new stories from HackerNews API'
-    )
-
-    parser.add_argument(
-        '-s',
-        '--shuffle',
-        nargs='?',
-        const=False,
-        type=bool,
-        help='Get the N new stories from HackerNews API'
-    )
-
-    parser.add_argument(
-        '-T',
-        '--threads',
-        nargs='?',
-        const=DEFAULT_THREADS_NUMBER,
-        type=int,
-        help='Determine the number max of threads'
-    )
-
-    options = parser.parse_args()
+    options = get_parser_options()
 
     if options.top_stories:
         param = options.top_stories, 'top'
